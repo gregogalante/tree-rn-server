36cc7e939d86d47575858c953af5fad516dc665d
Gregorio Galante <me@gregoriogalante.com>
2020-12-18 12:18:05 +0100

    General update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,12 +52,19 @@ class ApplicationController < MatildaCore::ApplicationController
     render_json_success(movies: [*1..10].map { get_random_pokemon })
   end
 
-  private def get_random_pokemon
+  private
+  
+  def get_random_pokemon
     {
       name: Faker::Games::Pokemon.name,
       location: Faker::Games::Pokemon.location,
       action: Faker::Games::Pokemon.move
     }
+  end
+
+  def create_auth_session(session_uuid, user_uuid)
+    auth_session = session_create(session_uuid, user_uuid)
+    auth_session
   end
 
 end
